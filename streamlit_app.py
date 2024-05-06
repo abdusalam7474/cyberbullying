82db370626bc986b607e1a922493199f17e8295c
abdusalam7474 <mideola824@gmail.com>
2024-05-06 23:06:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -9,8 +10,20 @@ import os
 
 st.title("Cyber Bullying detection App")
 st.write("Hi bully and the buliied")
+
+# download model from Dropbox, cache it and load the model into the app 
+@st.cache(allow_output_mutation=True)
+def download_model(url):
+  """Downloads a zipped model file from the specified URL using requests."""
+  model_response = requests.get(url)
+  model_response.raise_for_status()  # Raise error for failed downloads
+  return model_response.content
+
 model_path = os.path.dirname(__file__)
+model_url = "https://www.dropbox.com/scl/fi/3ifsodhw1dbo9kw8behl3/cyberbullying_dbert.zip?rlkey=g49hb39f8sc8j334wreqlonok&st=6v4iz3wn&dl=0" 
 try:
-  model = tf.saved_model.load(model_path)
+  #model = tf.saved_model.load(model_path)
+  model_bytes = download_model(model_url)
 except Exception as error:
   st.write(f"error: {error}")
+  
