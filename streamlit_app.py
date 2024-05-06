e36c1556c8dbd1b980801d5c521891e315887d8b
abdusalam7474 <mideola824@gmail.com>
2024-05-06 23:21:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,31 @@ def download_model(url):
   model_response.raise_for_status()  # Raise error for failed downloads
   return model_response.content
 
+def extract_model(model_bytes):
+  """Extracts the zipped model content into a BytesIO object representing an in-memory folder."""
+  import zipfile
+  model_zip = zipfile.ZipFile(BytesIO(model_bytes), 'r')
+  model_content = {}
+  for filename in model_zip.namelist():
+    file_content = model_zip.read(filename)
+    model_content[filename] = BytesIO(file_content)
+  return model_content
+
+def load_bert_model(model_content):
+  """Loads the BERT model from the extracted files in the model_content dictionary."""
+  from transformers import TFBertModel
+  # Identify the file path for the saved model (assuming it's consistent)
+  model_path = [path for path in model_content.keys() if 'saved_model.pb' in path][0]
+  reloaded_model = tf.saved_model.load(BytesIO(model_content[model_path]))
+  return reloaded_model
+
 model_path = os.path.dirname(__file__)
 model_url = "https://www.dropbox.com/scl/fi/3ifsodhw1dbo9kw8behl3/cyberbullying_dbert.zip?rlkey=g49hb39f8sc8j334wreqlonok&st=6v4iz3wn&dl=0" 
 try:
   #model = tf.saved_model.load(model_path)
   model_bytes = download_model(model_url)
+  # Extract the zipped model content
+  model_content = extract_model(model_bytes)
 except Exception as error:
   st.write(f"error: {error}")
   
