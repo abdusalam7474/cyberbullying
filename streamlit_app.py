bdc97fc1d566fd5df639459aeb3b86a5b1b9acbe
abdusalam7474 <mideola824@gmail.com>
2024-05-06 03:14:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ import tensorflow_text as text
 
 st.title("Cyber Bullying detection App")
 st.write("Hi bully and the buliied")
+model_path = "/"
 try:
   model = tf.saved_model.load(model_path)
 except error:
