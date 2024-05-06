416ef9924489992d268096f6a9f4ff1725fe6c48
abdusalam7474 <mideola824@gmail.com>
2024-05-06 03:08:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import shutil
+import tensorflow_hub as hub
+import tensorflow_text as text
 
 st.title("Cyber Bullying detection App")
 st.write("Hi bully and the buliied")
+try:
+  model = tf.saved_model.load(model_path)
+except error:
+  st.write(f"error: {error}")
